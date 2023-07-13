cb560a6eb7fbc6b6a163a8eb614d068ce531c281
buuhsmead <hdaems@redhat.com>
2023-07-13 10:03:53 +0200

    Works on my station

--- tls-certs/server_client.sh
+++ tls-certs/server_client.sh
@@ -5,9 +5,11 @@
 export OPENSSL_HOME=/usr/local/Cellar/openssl@3/3.1.1_1/
 export OPENSSL=${OPENSSL_HOME}/bin/openssl
 
+export DOMAIN=app-cees-cs.freubel.sandbox2538.opentlc.com
+export SAN="grpc-with-quarkus.${DOMAIN}"
+export NS_SCMP=app-cees-cs-smcp
 
-export SAN=grpc-with-quarkus.app-cees-bcu-app.apps.freubel.sandbox2538.opentlc.com
-export NS_SCMP=app-cees-bcu-smcp
+
 
 # Create a root certificate and private key to sign the certificates for your services:
 ${OPENSSL} req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -subj '/C=NL/O=BuuhSmead/CN=Self CA' -keyout CA.key -out CA.crt
@@ -24,6 +26,6 @@ ${OPENSSL} x509 -req -sha256 -days 365 -CA CA.crt -CAkey CA.key -set_serial 1 -i
 rm -rf client.csr
 
 
-oc delete secret igw-credential -n ${NS_SCMP}
-oc create -n ${NS_SCMP} secret generic igw-credential --from-file=tls.key=server.key --from-file=tls.crt=server.crt --from-file=ca.crt=CA.crt
+oc delete secret "igw-${NS_SCMP}" -n ${NS_SCMP}
+oc create -n ${NS_SCMP} secret generic "igw-${NS_SCMP}" --from-file=tls.key=server.key --from-file=tls.crt=server.crt --from-file=ca.crt=CA.crt
 curl -v --cacert CA.crt https://${SAN}/hello --cert client.crt --key client.key
