15f0d23033fcb3c6fb7c4ea4480b297d7be9a6e4
buuhsmead <hdaems@redhat.com>
2023-07-13 09:11:29 +0200

    greeting can be changed

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,9 +1,12 @@
 package org.acme;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 
 // http://localhost:8080/hello
@@ -12,10 +15,14 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
+
+    @ConfigProperty(name = "greeting.message")
+    String message;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        return "Hello from RESTEasy Reactive " + message;
     }
 }
 
